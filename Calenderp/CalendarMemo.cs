2ec97a56347159eb1883373190117325b9d1d4c7
benthescott <benjamin.cline@millcreeksoftware.biz>
2016-12-06 17:09:23 -0600

    Added CalendarMemo class

---
+++ Calenderp/CalendarMemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calenderp
+{
+    class CalendarMemo
+    {
+        public CalendarMemo(int submittedDay, int submittedMonth, int submittedYear, string submittedMemoTitle, string submittedMemoDescription)
+        {
+            day = submittedDay;
+            month = submittedMonth;
+            year = submittedYear;
+            memoTitle = submittedMemoTitle;
+            memoDescription = submittedMemoDescription;
+        }
+
+        int day;
+        int month;
+        int year;
+        string memoTitle;
+        string memoDescription;
+    }
+}
--- Calenderp/MainPage.xaml.cs
+++ Calenderp/MainPage.xaml.cs
@@ -348,22 +348,4 @@ namespace Calenderp
         string eventTitle;
         //bool reminder
     }
-
-    public class CalendarMemo
-    {
-        public CalendarMemo(int submittedDay, int submittedMonth, int submittedYear, string submittedMemoTitle, string submittedMemoDescription)
-        {
-            day = submittedDay;
-            month = submittedMonth;
-            year = submittedYear;
-            memoTitle = submittedMemoTitle;
-            memoDescription = submittedMemoDescription;
-        }
-
-        int day;
-        int month;
-        int year;
-        string memoTitle;
-        string memoDescription;
-    }
 }
